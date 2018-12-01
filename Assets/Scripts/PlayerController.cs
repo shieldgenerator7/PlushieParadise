6890b4660f4fc808b900b1a90c4caf634e64ac95
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-01 17:34:55 -0500

    You can now just set the jump height

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     //Settings
     public float movementSpeed = 3;
-    public float baseJumpForce = 2;
-    public float jumpDuration = 1.1f;
+    public float jumpHeight = 2.1f;//how high she can jump
+    public float jumpDuration = 0.5f;//how long it takes her to get to max jump height
 
     //Static Variables
     private float jumpDecay = 0;
+    public float baseJumpForce = 0;
     //Runtime vars
     private float jumpForce;
     private float jumpStartTime;
@@ -22,13 +23,15 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        //Set base jump force
+        baseJumpForce = (jumpHeight / jumpDuration);// + Physics2D.gravity.magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        rb2d.velocity = new Vector2(horizontal * movementSpeed * Time.deltaTime, rb2d.velocity.y);
+        rb2d.velocity = new Vector2(horizontal * movementSpeed, rb2d.velocity.y);
         if (Input.GetButton("Jump"))
         {
             if (jumpStartTime == 0)
@@ -38,8 +41,8 @@ public class PlayerController : MonoBehaviour
             }
             if (Time.time <= jumpStartTime + jumpDuration)
             {
-                jumpForce = (1 - ((Time.time - jumpStartTime) / jumpDuration)) * baseJumpForce;
-                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce * Time.deltaTime);
+                //jumpForce = (1 - ((Time.time - jumpStartTime) / jumpDuration)) * baseJumpForce;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
             }
         }
         else if (Input.GetButtonUp("Jump"))
