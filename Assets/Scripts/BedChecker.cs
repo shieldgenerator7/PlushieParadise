98b5b05ef5f5cc4e53a20566c4c1f7376fd18ce6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-02 03:47:54 -0500

    Added bed as a spawn point

---
+++ Assets/Scripts/BedChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Used to spawn the player at a certain location
+public class BedChecker : MonoBehaviour {
+
+    public GameObject playerSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        GameObject.FindGameObjectWithTag("Player").transform.position = playerSpawnPoint.transform.position;
+        Destroy(this);
+	}
+}
