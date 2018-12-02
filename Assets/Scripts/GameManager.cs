7e59aafedb62bb03764490faa7a2ec951e69fe21
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-02 03:57:55 -0500

    Added GameManager that loads levels

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public string firstLevelName = "Level1";
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene(firstLevelName, LoadSceneMode.Additive);
+	}
+}
