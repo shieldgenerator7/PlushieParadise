5aaea53f9f4e30f54ef6c8afc35dac3d6a77b201
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-02 12:53:58 -0500

    Plushies get put in a pen during level transition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public string firstLevelName = "Level1";
+    public GameObject plushiePen;
 
     private string currentLevelName;
 
     private static GameManager instance;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         currentLevelName = firstLevelName;
         SceneManager.LoadScene(firstLevelName, LoadSceneMode.Additive);
         if (instance == null)
         {
             instance = this;
         }
-	}
+    }
 
     public static void loadNextLevel(string levelName)
     {
@@ -29,9 +32,22 @@ public class GameManager : MonoBehaviour {
         {
             SceneManager.UnloadSceneAsync(current);
         }
+        //Put plushies "away"
+        instance.putAwayPlushies();
         //Update current
         instance.currentLevelName = current = levelName;
         //Load current level
         SceneManager.LoadScene(current, LoadSceneMode.Additive);
+    }
+
+    /// <summary>
+    /// Puts plushies in a safe place where they won't interfere with the level
+    /// </summary>
+    void putAwayPlushies()
+    {
+        foreach (GameObject plushie in GameObject.FindGameObjectsWithTag("Plushie"))
+        {
+            plushie.transform.position = plushiePen.transform.position;
+        }
     }
 }
