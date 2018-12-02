b2a13c977aea5d3c7dd7360463f7294c2b98606e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-01 23:40:03 -0500

    First version of naming the plushies

---
+++ Assets/Scripts/NameUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameUpdater : MonoBehaviour {
+
+    public InputField inputField;
+
+    private GameObject plushie;
+
+    private static NameUpdater instance;
+    
+	void Awake () {
+		if (instance == null)
+        {
+            instance = this;
+        }
+        inputField.onEndEdit.AddListener(acceptName);
+	}
+	
+    void acceptName(string name)
+    {
+        plushie.name = name;
+        inputField.gameObject.SetActive(false);
+    }
+
+	public static void getNameForNewPlushie(GameObject newPlushie)
+    {
+        instance.inputField.gameObject.SetActive(true);
+        instance.plushie = newPlushie;
+    }
+}
---
+++ Assets/Scripts/NewPlushie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewPlushie : MonoBehaviour {
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            NameUpdater.getNameForNewPlushie(gameObject);
+            Destroy(this);
+        }
+    }
+}
