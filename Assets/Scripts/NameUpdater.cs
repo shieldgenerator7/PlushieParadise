e5b4a14aee418ef00d6096befbee17994cccccf5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-02 13:07:42 -0500

    Added text message to plushie canvas

--- Assets/Scripts/NameUpdater.cs
+++ Assets/Scripts/NameUpdater.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NameUpdater : MonoBehaviour {
+public class NameUpdater : MonoBehaviour
+{
 
+    public Canvas plushieCanvas;
     public InputField inputField;
+    public Text messageText;
+
+    private string originalMessage;
 
     private GameObject plushie;
 
     private static NameUpdater instance;
-    
-	void Awake () {
-		if (instance == null)
+
+    void Awake()
+    {
+        if (instance == null)
         {
             instance = this;
         }
         inputField.onEndEdit.AddListener(acceptName);
-	}
+    }
 
     public static void getNameForNewPlushie(GameObject newPlushie)
     {
-        instance.inputField.gameObject.SetActive(true);
+        instance.plushieCanvas.gameObject.SetActive(true);
         instance.inputField.ActivateInputField();
         instance.plushie = newPlushie;
+        //Update Message Text
+        instance.originalMessage = instance.messageText.text;
+        instance.messageText.text += newPlushie.name + "!";
         //Pause game
         Time.timeScale = 0;
     }
@@ -32,7 +41,8 @@ public class NameUpdater : MonoBehaviour {
     {
         plushie.name = name;
         inputField.text = "";
-        inputField.gameObject.SetActive(false);
+        messageText.text = originalMessage;
+        plushieCanvas.gameObject.SetActive(false);
         //Resume game
         Time.timeScale = 1;
     }
